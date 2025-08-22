0902f6c06d3b3c54fbe7685df0406c8a40f03f8f
osaid <osaidmuhammad6@gmail.com>
2025-08-22 21:56:22 +0100

    Add initial functions with menu options

---
+++ FlashcardQuizApp.py
@@ -0,0 +1,23 @@
+def addFlashCards():
+    print("you're on add flash card section")
+
+
+def reviewFlashCards():
+    print("you're on review flash card section")
+
+
+def takeQuiz():
+    print("you're on quiz flash card section")
+
+
+def deleteFlashCards():
+    print("you're on delete flash card section")
+
+
+def menu():
+    print("Welcome to Flash Quiz, ready to learn?")
+    navigationOptions = input(
+        "1.Add Flash Card\n2.Review Flashcards\n3.Take a Quiz\n4.Delete Flashcards\n")
+
+
+menu()
