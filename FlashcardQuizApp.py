2dd1249bd2e253503b8d270192c1ec04038c37c1
osaid <osaidmuhammad6@gmail.com>
2025-08-23 09:36:18 +0100

    Implement addFlashCards function and update menu navigation

--- FlashcardQuizApp.py
+++ FlashcardQuizApp.py
@@ -1,5 +1,49 @@
+import json
+import os
+
+FILENAME = 'Flashcards.json'
+
+
 def addFlashCards():
     print("you're on add flash card section")
+    options = int(input("1.Add Flash Card\n2.Return to menu\n"))
+
+    if options == 1:
+        addQuestion = input("Please input the question: ")
+        addAnswer = input("Please input the answer: ")
+
+        if not os.path.exists(FILENAME) or os.stat(FILENAME).st_size == 0:
+            data = []
+
+        else:
+            with open(FILENAME, "r") as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = []
+        if data:
+            newId = data[-1]["ID"] + 1
+        else:
+            newId = 1
+
+        flascards = {
+            "ID": newId,
+            "Question": addQuestion,
+            "Answer": addAnswer
+        }
+
+        data.append(flascards)
+
+        with open(FILENAME, "w") as file:
+            json.dump(data, file, indent=4)
+
+        print(f"Flash card {newId} added successfully!")
+
+    elif options == 2:
+        return
+    else:
+        print("Invalid Option")
+        return
 
 
 def reviewFlashCards():
@@ -16,8 +60,25 @@ def deleteFlashCards():
 
 def menu():
     print("Welcome to Flash Quiz, ready to learn?")
-    navigationOptions = input(
-        "1.Add Flash Card\n2.Review Flashcards\n3.Take a Quiz\n4.Delete Flashcards\n")
+    while True:
+
+        navigationOptions = int(input(
+            "1.Add Flash Card\n2.Review Flashcards\n3.Take a Quiz\n4.Delete Flashcards\n"))
+
+        if navigationOptions == 1:
+            addFlashCards()
+        elif navigationOptions == 2:
+            reviewFlashCards()
+        elif navigationOptions == 3:
+            takeQuiz()
+        elif navigationOptions == 4:
+            deleteFlashCards()
+        else:
+            print("Invalid Input")
+            continue
+
+        if not navigationOptions:
+            break
 
 
 menu()
